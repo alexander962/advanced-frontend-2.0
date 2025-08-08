984cdbec769a3a69cccccf07080446ce6cbaabd9
alexander962 <sanek12011996@gmail.com>
2025-08-08 09:19:40 +0300

    webpack initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  // стартовая точка нашего приложения
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  // настройки куда и как мы будем делать сборку нашего приложения
+  output: {
+    // главный файл сборки нашего приложения
+    filename: '[name].[contenthash].js',
+    //путь, куда сборка должна происходить
+    path: path.resolve(__dirname, 'build'),
+    // подчищает ненужные файлы
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // используем наш index.html файл как шаблон
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new webpack.ProgressPlugin(),
+  ]
+}
